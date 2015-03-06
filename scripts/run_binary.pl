91fb32a35254f9f8a802ea7dd8218c54c5cf0eef
Albert Vilella <avilella@gmail.com>
2015-03-06 17:47:13 +0000

    change to inputfile

--- scripts/run_binary.pl
+++ scripts/run_binary.pl
@@ -1,7 +1,18 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use IPC::Open3 'open3'; $SIG{CHLD} = 'IGNORE';
+use Symbol 'gensym';
+use Getopt::Long;
 
-print `./bin/hello_world`;
+my $inputfile;
+my $debug; my $verbose;
+GetOptions(
+	   'i|input|inputfile:s' => \$inputfile,
+           'debug' => \$debug,
+          'verbose' => \$verbose,
+          );
+
+print `$inputfile`;
 
 1;
